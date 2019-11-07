3cc9a2c45226e55efbfc9b064e33d5f4ace83ef4
gg1140 <gordonlschan@gmail.com>
2019-11-07 15:16:32 -0800

    Adding config.py file & Initiate Flask App in __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from config import Config
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(Config)
+
+    with app.app_context():
+        # import routes and db
+        return app
--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 """Flask app configuration"""
 
+
 class Config:
     """General Config"""
+    APP_DIR = os.path.dirname(os.path.realpath(__file__))
     #TESTING = True
     #DEBUG = True
     #SECRET_KEY = ''
     #SESSION_COOKIE_NAME = 'my_cookie'
 
     """Database"""
-    APP_DIR = os.path.dirname(os.path.realpath(__file__))
-    DB_URI = "sqlite:///" + os.path.join(APP_DIR, 'app/recipe_app.db')
+    DATABASE_URI = "sqlite:///" + os.path.join(APP_DIR, 'app/recipe_app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    print(os.path.dirname(__file__))
